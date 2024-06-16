7704aa581c23d9a64a767c9caf855a43ac7db005
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-06-15 17:04:49 -0700

    message

--- js-file.js
+++ js-file.js
@@ -36,46 +36,45 @@
         
 
         function playRound(humanSelection, computerChoice){
-            if (humanSelection == "Rock" && computerChoice == "Scissors") {
+            if (humanSelection == "You Chose Rock" && computerChoice == "Scissors") {
                 humanScore = ++humanScore; 
                 alert("You win rock beats Scissors");
             } 
-            else if (humanSelection == "Paper" && computerChoice == "Rock") {
+            else if (humanSelection == "You Chose Paper" && computerChoice == "Rock") {
                 alert("You win Paper beats Rock");
                 humanScore = ++humanScore;
             }
-            else if (humanSelection == "Scissors" && computerChoice == "Paper"){
+            else if (humanSelection == "You Chose Scissors" && computerChoice == "Paper"){
                 alert("You win Scissors beats Paper");
                 humanScore = ++humanScore;
             }
-            else if (humanSelection == "Scissors" && computerChoice ==  "Rock"){
+            else if (humanSelection == "You Chose Scissors" && computerChoice ==  "Rock"){
                 alert("You lose Rock beats Scissors");
                 computerScore = ++computerScore;
             }
-            else if (humanSelection == "Rock" && computerChoice == "Paper"){
+            else if (humanSelection == "You Chose Rock" && computerChoice == "Paper"){
                 alert("You lose Paper beats Rock");
                 computerScore = ++computerScore
             }
-            else if (humanSelection == "Paper" && computerChoice == "Scissors"){
+            else if (humanSelection == "You Chose Paper" && computerChoice == "Scissors"){
                 alert("You lose Scissors beats Paper");
                 computerScore = ++computerScore
             }
-            else if (humanSelection == "Paper" && computerChoice == "Paper"){
+            else if (humanSelection == "You Chose Paper" && computerChoice == "Paper"){
                 alert(`It is a draw Human chose ${humanScore} vs Computer: ${computerScore}`)
             }
-            else if (humanSelection == "Rock" && computerChoice == "Rock"){
+            else if (humanSelection == "You Chose Rock" && computerChoice == "Rock"){
                 alert(`It is a draw Human chose' ${humanScore} vs Computer: ${computerScore}`)
             }
-            else (humanSelection == "Scissors" && computerChoice == "Scissors");{
+            else (humanSelection == "You Chose Scissors" && computerChoice == "Scissors");{
                 alert(`It is a draw Human chose ${humanScore} vs Computer: ${computerScore}`)
             }
         }
-        console.log(humanSelection);
         
         
         
         const computerSelection = getComputerChoice;
-        //playRound(humanSelection, computerSelection);
+        playRound(humanSelection, computerSelection);
 
 
 
