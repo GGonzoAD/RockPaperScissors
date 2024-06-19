64e9a0451a7a77d1ac35984cdc944609c6f84c34
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-06-19 08:54:31 -0700

    message

--- js-file.js
+++ js-file.js
@@ -22,37 +22,37 @@ button.addEventListener("click", function(event) {
         }
 
             function playRound(humanSelection, computerChoice){
-            if (humanSelection == "You Chose Rock" && computerChoice == "Scissors") {
+            if (humanSelection === "You Chose Rock" && computerChoice === "Scissors") {
             humanScore = ++humanScore; 
             alert("You win Rock beats Scissors");
                 
-            } else if (humanSelection == "You Chose Paper" && computerChoice == "Rock") {
+            } else if (humanSelection === "You Chose Paper" && computerChoice === "Rock") {
               alert("You win Paper beats Rock");
               humanScore = ++humanScore;
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Scissors" && computerChoice == "Paper"){
+            } else if (humanSelection === "You Chose Scissors" && computerChoice === "Paper"){
               alert("You win Scissors beats Paper");
               humanScore = ++humanScore;
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Scissors" && computerChoice ==  "Rock"){
+            } else if (humanSelection === "You Chose Scissors" && computerChoice ===  "Rock"){
               alert("You lose Rock beats Scissors");
               computerScore = ++computerScore;
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Rock" && computerChoice == "Paper"){
+            } else if (humanSelection === "You Chose Rock" && computerChoice === "Paper"){
               alert("You lose Paper beats Rock");
               computerScore = ++computerScore;
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Paper" && computerChoice == "Scissors"){
+            } else if (humanSelection === "You Chose Paper" && computerChoice === "Scissors"){
               alert("You lose Scissors beats Paper");
               computerScore = ++computerScore;
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Paper" && computerChoice == "Paper"){
+            } else if (humanSelection === "You Chose Paper" && computerChoice === "Paper"){
               alert(`It is a draw Human chose ${humanScore} vs Computer: ${computerScore}`);
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Rock" && computerChoice == "Rock"){
+            } else if (humanSelection === "You Chose Rock" && computerChoice === "Rock"){
               alert(`It is a draw Human chose' ${humanScore} vs Computer: ${computerScore}`);
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
-            } else if (humanSelection == "You Chose Scissors" && computerChoice == "Scissors");{
+            } else if (humanSelection === "You Chose Scissors" && computerChoice === "Scissors");{
               alert(`It is a draw Human chose ${humanSelection} vs Computer: ${computerChoice}`);
               alert(`Human: ${humanScore} vs Computer: ${computerScore}`);
             }
